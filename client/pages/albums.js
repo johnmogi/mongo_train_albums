f77188f2ed610f6642288605613280177c6390f9
johnmogi <anguru@gmail.com>
2021-10-28 11:05:48 +0300

    nextJs

---
+++ client/pages/albums.js
@@ -0,0 +1,37 @@
+// albums will be populated at build time by getStaticProps()
+function List({ albums }) {
+  return (
+    <table>
+      <tr>
+        {albums.map((album, i) => (
+          <tr key={i} id={i}>
+            <td>{album.albumName}</td>
+            <td>{album.albumImage}</td>
+            <td>{album.artist}</td>
+            <td>{album.date}</td>
+          </tr>
+        ))}
+      </tr>
+    </table>
+  );
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch("http://localhost:3000/api/albums");
+  const albums = await res.json();
+
+  // By returning { props: { albums } }, the Blog component
+  // will receive `albums` as a prop at build time
+  return {
+    props: {
+      albums,
+    },
+  };
+}
+
+export default List;
---
+++ nextJS-master.md
@@ -0,0 +1,13 @@
+https://nextjs.org/docs/getting-started
+
+
+npx create-next-app@latest
+ cd client
+
+ change port
+ https://onecompiler.com/posts/3w5u9patd/how-to-change-port-in-next-js-app
+   "scripts": {
+    "dev": "next dev -p 3005",
+|| npm run dev -- -p 3005
+
+
